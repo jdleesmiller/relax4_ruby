40d41f9684fff129948d1ddee39ec8e2ca5689f6
John Lees-Miller <jdleesmiller@gmail.com>
2010-09-07 15:51:58 +0100

    new test case that fails

--- test/test_relax4.rb
+++ test/test_relax4.rb
@@ -32,125 +32,42 @@ class TestRelax4< Test::Unit::TestCase
     assert_equal soln, Relax4.solve(prob.merge(:auction_init => true))
   end
 
+  # Load problem from relax4_input format.
+  def problem_from_relax4_inp inp
+    inp_lines = inp.lines.to_a
+    raise "empty input" if inp_lines.empty?
+
+    head = inp_lines.first.scan(/^\s*(\d+)\s+(\d+)\s*$/)[0] or
+      raise "bad first line: expected number of nodes and number of arcs"
+    num_nodes, num_arcs = head.map{|x| x.to_i}
+
+    arcs = inp_lines[1..num_arcs].map{|l| l.scan(
+      /^\s*(\d+)\s+(\d+)\s+(-?\d+)\s+(\d+)\s*$/)[0] or raise "bad arc: #{l}"}
+
+    demands = inp_lines[1+num_arcs..num_arcs+num_nodes].map{|l| -l.to_i}
+
+    {
+      :demands     => demands,
+      :start_nodes => arcs.map{|x| x[0].to_i},
+      :end_nodes   => arcs.map{|x| x[1].to_i},
+      :costs       => arcs.map{|x| x[2].to_i},
+      :capacities  => arcs.map{|x| x[3].to_i}
+    }
+  end
+
   def test_solve_3
-    # This is the RELAX4.INP file packaged with the RELAX IV code.
-    arc_text = <<ARCS
-       1      10      -4     215
-       1      16      -8     138
-       8      18       2     215
-       8      14      -5      76
-       8      20      -6     193
-      10       8      -9     215
-      10      16      -4     215
-      10       6       9     190
-      10      20      -8     141
-      10       4      -5     152
-      10      11       2      63
-       2      15       5     266
-       2       4      -5     186
-       2       6      -3     162
-       2      17       7      84
-       2      11       6      94
-       2      18      -6     113
-      12      18      -6     266
-      12       7       9     106
-      12       9       4     143
-      12      17       7      95
-      12      15       7     181
-      12       4       9     104
-      12      20      -9      84
-      15      12       8     266
-      15      16      -4     266
-      15      17       5     154
-      15      20       3     132
-      15       4       2     164
-       3       6       2      50
-       3      18      -7     131
-       3      19      -8      89
-       6       7      -2      50
-       6      19      -6     102
-       6       9       6      56
-       7       9      -7      50
-       7       6       3     129
-       7       5      -7     141
-       9      19      -1      50
-       9      17      -2      50
-       9      18       4      50
-       9      13      -7     162
-       9      16      -2     121
-       9      11      10     115
-       9      10      -3     166
-       9       4       1     111
-       4      13     -10     195
-       4      19       8     112
-       4      16       2     195
-       4      10      -9     143
-       4       9     -10     135
-       4      14      -4     191
-      13      18      -9     195
-      13      19     -10     195
-      13       8      -8     165
-      13      17       0     132
-       5      11      -8     319
-       5       7      -8      70
-       5      20       0      63
-       5       9      -9     194
-      11      14      -3     319
-      11      19       2     125
-      11      12      -9     177
-      14      20      -7     319
-      14      16       7     319
-      14       8       9     174
-      14      11      10      77
-      14       5      -4     123
-      14      18       5     125
-      14      12     -10      69
-      16      19      -3     128
-      16      10      -4      56
-      16       6       0     145
-      16      17      10     104
-      16      12       2     199
-      16       5      -5     159
-      17       7       2      68
-      17      12      -3      57
-      17      18      -3     113
-      17      11      -2     130
-ARCS
-  node_text = <<NODES # note: these are supplies (= -ve demands)
-     215
-     266
-       5
-     195
-     319
-       0
-       0
-       0
-       0
-       0
-       0
-       0
-       0
-       0
-       0
-    -386
-      -1
-    -399
-     -54
-    -160
-NODES
-    arc_rx = /^\s+(\d+)\s+(\d+)\s+(-?\d+)\s+(\d+)\s*$/
-    prob = {
-      :demands     => node_text.strip.split(/\s+/m).map{|x| -x.to_i},
-      :start_nodes => arc_text.scan(arc_rx).map{|x| x[0].to_i},
-      :end_nodes   => arc_text.scan(arc_rx).map{|x| x[1].to_i},
-      :costs       => arc_text.scan(arc_rx).map{|x| x[2].to_i},
-      :capacities  => arc_text.scan(arc_rx).map{|x| x[3].to_i}}
+    prob = problem_from_relax4_inp File.read('test/RELAX4.INP')
 
     flows = Relax4.solve(prob)
     assert_equal -26464, flows.zip(prob[:costs]).map{|f,c|f*c}.inject(:+)
 
     flows = Relax4.solve(prob.merge(:auction_init => true))
     assert_equal -26464, flows.zip(prob[:costs]).map{|f,c|f*c}.inject(:+)
+  end
+
+  def test_solve_4
+    prob = problem_from_relax4_inp File.read('test/test_solve_4.inp')
+    flows = Relax4.solve(prob)
   end
 
   def test_solve_lemon_1
